dc399a6af7d6396af87cded98e2c8b0b53614b29
JJFairchild <jonas.fairchild@ucas-edu.net>
2026-01-21 13:43:32 -0700

    Variables notes

--- Notes/variables.py
+++ Notes/variables.py
@@ -1,2 +1,14 @@
 name = "Martha"
+
+time = "1:29 PM"
+
+pi = 3.1416926535
+
+room = 201
+
+pie = input("What is your favorite kind of pie?: ")
+
 print(name)
+print(room)
+print("Programming is the best!")
+print(time, "is", pie, "pie time!")
