342bc42c4906909922bf44a1d7e97a34e459cba6
JJFairchild <jonas.fairchild@ucas-edu.net>
2026-01-23 13:44:08 -0700

    Integers and Floats

---
+++ Notes/integers_floats.py
@@ -0,0 +1,12 @@
+#JF Integers and Floats
+
+"""price = float(input("How much does the item cost? $"))
+tax_rate = float(input("What is the tax percent? %"))/100
+
+total = price * (tax_rate + 1)
+print(round(total, 2))"""
+
+apples = 300
+friends = 65
+
+print(f"Each friend gets {apples//friends} apples and there will be {apples%friends} left over.")
