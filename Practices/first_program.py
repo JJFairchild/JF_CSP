50a8807e07912a6f07ab918f38a8606e263d423a
JJFairchild <jonas.fairchild@ucas-edu.net>
2026-01-23 19:18:21 +0000

    nothing burger

--- Practices/first_program.py
+++ Practices/first_program.py
@@ -5,23 +5,9 @@ tens = ["", "vigin", "trigin", "quadragin", "quinquagin", "sexagin", "septagin",
 hundreds = ["", "cent", "duocent", "trecent", "quadringent", "quingent", "sescent", "septingent", "octingent", "nongent"]
 
 
-num = int(input("Enter a number of any size: "))
-string = "e" + str(num)
-location = string.find("e")-len(string)
+num = input("Enter a number of any size: ")
 
-groups = []
-i = -1
-while abs(i) < len(str(string)):
-    
-    if location in range(i, i-3, -1):
-        pass
 
-    groups.append(int(string[i]+string[i-1]+string[i-2]))
-    i -= 3
-
-for group in groups:
-    one = num % 10
-    ten = num // 10 % 10
-    hundred = num // 100 % 10
-
-    print(hundred, ten, one)
+for i in range(len(num)//3):
+    i *= -3
+    print(num[i-2]+num[i-1]+num[i])
