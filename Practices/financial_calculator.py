5c7ee332855bd86fcd3c6613912e127008304a98
JJFairchild <jonas.fairchild@ucas-edu.net>
2026-01-26 13:45:46 -0700

    broken fc

--- Practices/financial_calculator.py
+++ Practices/financial_calculator.py
@@ -1,15 +1,11 @@
 # JF Financial Calculator
 
-income = float(input("What is your monthly income? $"))
-rent = float(input("What is your monthly rent/mortgage? $"))
-utilities = float(input("What is your monthly utilities cost? $"))
-groceries = float(input("What is your monthly groceries budget? $"))
-transport = float(input("What is your monthly transport budget? $"))
-
-print(f"\nYour rent is ${rent}, which is {round(rent/income*100, 2)}% of your income.")
-print(f"Your utilites are ${utilities}, which is {round(utilities/income*100, 2)}% of your income.")
-print(f"Your groceries are ${groceries}, which is {round(groceries/income*100, 2)}% of your income.")
-print(f"Your transportation is ${transport}, which is {round(transport/income*100, 2)}% of your income.")
-print(f"You should be saving ${income/10} per month, which is 10% of your income.")
-
-print(f"\nWith this budget plan, you will have {income*9/10-rent-utilities-groceries-transport} of spending money per month.")
+p = []
+for j in ["What is your monthly income? $", "What is your monthly rent/mortgage? $", "What is your monthly utilities cost? $", "What is your monthly groceries budget? $", "What is your monthly transport budget? $"]:
+    p.append(float(input(j)))
+i = float(input())
+r = float(input())
+u = float(input())
+g = float(input())
+t = float(input())
+print(f"\nYour rent is ${p[1]}, which is {round(p[1]/p[0]*100,2)}% of your income.\nYour utilites are ${p[2]}, which is {round(p[2]/p[0]*100, 2)}% of your income.\nYour groceries are ${p[3]}, which is {round(p[3]/p[0]*100, 2)}% of your income.\nYour transportation is ${p[4]}, which is {round(p[4]/p[0]*100, 2)}% of your income.\nYou should be saving ${p[0]/10} per month, which is 10% of your income.\n\nWith this budget plan, you will have {p[0]*9/10-p[1]-p[2]-p[3]-p[4]} of spending money per month.")
