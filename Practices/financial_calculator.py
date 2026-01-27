04a8acfea0306e7ff1b08d493659d23b156530aa
JJFairchild <jonas.fairchild@ucas-edu.net>
2026-01-27 18:46:11 +0000

    Golf coded Financial Calculator

--- Practices/financial_calculator.py
+++ Practices/financial_calculator.py
@@ -1,11 +1,8 @@
 # JF Financial Calculator
-
-p = []
-for j in ["What is your monthly income? $", "What is your monthly rent/mortgage? $", "What is your monthly utilities cost? $", "What is your monthly groceries budget? $", "What is your monthly transport budget? $"]:
-    p.append(float(input(j)))
-i = float(input())
-r = float(input())
-u = float(input())
-g = float(input())
-t = float(input())
-print(f"\nYour rent is ${p[1]}, which is {round(p[1]/p[0]*100,2)}% of your income.\nYour utilites are ${p[2]}, which is {round(p[2]/p[0]*100, 2)}% of your income.\nYour groceries are ${p[3]}, which is {round(p[3]/p[0]*100, 2)}% of your income.\nYour transportation is ${p[4]}, which is {round(p[4]/p[0]*100, 2)}% of your income.\nYou should be saving ${p[0]/10} per month, which is 10% of your income.\n\nWith this budget plan, you will have {p[0]*9/10-p[1]-p[2]-p[3]-p[4]} of spending money per month.")
+s=0
+for j in ["income", "rent/mortgage", "utilities cost", "groceries budget", "transport budget"]:
+    p=float(input(f"What is your monthly {j}? $"))
+    s+=p
+    if j=="income": i=p
+    print(f"Your {j} is {round(p/i*100,2)}% of your income.\n")
+print(f"You should be saving ${i/10} per month, which is 10% of your income.\n\nWith this budget plan, you will have {i*1.9-s} of spending money per month.")
