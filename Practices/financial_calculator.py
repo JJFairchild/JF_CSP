798d2fd600c7bd6451c24dd295d282e03b1943f5
JJFairchild <jonas.fairchild@ucas-edu.net>
2026-02-06 21:19:03 +0000

    Financial Calculator Update

--- Practices/financial_calculator.py
+++ Practices/financial_calculator.py
@@ -16,7 +16,7 @@ You have $ 550.00 of spending money each month!
 """
 
 def g(p): return round(float(input(f"\nWhat is your monthly {p}? $")),2)
-def c(m,t): print(f"Your {m} is {g(m)/t*100}% of your income.")
+def c(m,v,t): print(f"Your {m} is {v/t*100}% of your income.")
 i=g("income")
-for m in ["rent/mortgage","utilities","groceries","transportation"]: c(m,i)
-print(f"You should save ${i/10} a month, that is 10% of your income.\nYou have ${i}")
+p=i-sum(c(m,v:=g(m),i) or v for m in ["rent", "utilities", "groceries", "transportation"])
+print(f"\nYou should save ${i/10} a month, that is 10% of your income.\n\nYou have ${p-i/10} of spending money each month!")
