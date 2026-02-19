9fb33c403f3ce0b515d3f81c802c5742d78b48c1
JJFairchild <jonas.fairchild@ucas-edu.net>
2026-02-19 13:44:55 -0700

    Unfinished variables practice

---
+++ C/Practices/variables.c
@@ -0,0 +1,18 @@
+// JF, Variables Practice
+
+#include <stdio.h>
+
+int main(){
+    char name[] = "Sigrid";
+    int age = 14;
+    char eye_color[] = "Green";
+    char favorite_color[] = "Red";
+    char breakfast[] = "Dagmal";
+    char school[] = "Valhalla High";
+    float GPA = 3.67;
+    char favorite_subject[] = "Building";
+    int year = 964;
+    int num = 7;
+
+    return 0;
+}
