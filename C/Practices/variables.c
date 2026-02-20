5ef1f01935b5f25d19d4f41601c04b49526a0aa7
JJFairchild <jonas.fairchild@ucas-edu.net>
2026-02-20 12:56:28 -0700

    Variables Practice

--- C/Practices/variables.c
+++ C/Practices/variables.c
@@ -9,10 +9,21 @@ int main(){
     char favorite_color[] = "Red";
     char breakfast[] = "Dagmal";
     char school[] = "Valhalla High";
-    float GPA = 3.67;
+    float GPA = 3.67f;
     char favorite_subject[] = "Building";
     int year = 964;
     int num = 7;
+
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Eye color: %s\n", eye_color);
+    printf("Favorite color: %s\n", favorite_color);
+    printf("Breakfast: %s\n", breakfast);
+    printf("School: %s\n", school);
+    printf("GPA: %f\n", GPA);
+    printf("Favorite subject: %s\n", favorite_subject);
+    printf("Year: %d\n", year);
+    printf("Number: %d\n", num);
 
     return 0;
 }
