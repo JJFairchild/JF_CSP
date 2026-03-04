b16f2173ade090c1bb1ad30781ae545b714792c6
JJFairchild <jonas.fairchild@ucas-edu.net>
2026-03-04 13:44:49 -0700

    Loops, arrays, & FizzBuzz

--- C/Notes/arrays.c
+++ C/Notes/arrays.c
@@ -1,12 +1,37 @@
 // JF Lists Notes
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(){
     int grades[] = {84, 74, 99, 78, 97};
     printf("%d\n", grades[2]);
     grades[2] = 85;
     printf("%d\n", grades[2]);
+
+    float distance[] = {72.1, 66.3, 45.2, 27.45};
+
+    char names[][20] = {"Alex", "Katie", "Andrew", "Vienna", "Tia", "Treyson", "Xavier", "Jake"};
+    printf("%s\n", names[5]);
+
+    for(int i = 20; i>=0; i--){
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    int grade_len = sizeof(grades) / sizeof(grades[0]);
+    for(int i=0; i<grade_len; i++){
+        printf("%d ", grades[i]+5);
+    }
+    printf("\n");
+
+    srand(time(NULL));
+    int goose = rand()%15+1;
+    for(int count=1; count<goose; count++){
+        printf("Duck...\n");
+    }
+    printf("GOOSE!\n");
 
     return 0;
 }
---
+++ C/Practices/fizzbuzz.c
@@ -0,0 +1,10 @@
+// JF FizzBuzz
+
+#include <stdio.h>
+int main(){
+    for(int i=1; i<=50; i++){
+        if(i%3==0){printf("Fizz");}
+        if(i%5==0){printf("Buzz");}
+        else if(i%3!=0){printf("%d",i);}
+        printf("\n");}
+    return 0;}
